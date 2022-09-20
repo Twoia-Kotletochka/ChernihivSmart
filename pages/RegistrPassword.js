1db2fbf89e6113a562535d7af322ec6a9a1a5ce5
Parazit15D <igorlyons@gmail.com>
2022-09-20 13:14:28 +0300

    RegistrPassword

---
+++ pages/RegistrPassword.js
@@ -0,0 +1,47 @@
+import { Text, View, StatusBar, TextInput, KeyboardAvoidingView, Image, TouchableHighlight, TouchableOpacity } from 'react-native'
+import { signupStyle } from '../styles/signup'
+import { useState } from 'react'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+
+
+export default function RegistrPassword() {
+    const [number, onChangeText] = useState("");
+
+    return (
+        <KeyboardAvoidingView
+            style={signupStyle.container}
+        >
+            <Image source={require("../assets/logo.png")} style={signupStyle.img} />
+            <View style={signupStyle.box}>
+                <Text style={[signupStyle.text, { fontSize: 35 }]}>Створіть пароль</Text>
+                <TextInput
+                    style={signupStyle.input}
+                    onChangeText={onChangeText}
+                    secureTextEntry={true}
+                    keyboardType='password'
+                    value={number}
+                />
+
+                <TextInput
+                    style={signupStyle.input}
+                    onChangeText={onChangeText}
+                    secureTextEntry={true}
+                    keyboardType='password'
+                    value={number}
+                />
+
+                <Text style={[signupStyle.text, { fontSize: 12 }]}>Введіть пароль два рази</Text>
+            </View>
+            <TouchableOpacity
+                style={signupStyle.button}>
+                <Text style={{ color: 'white', fontSize: 22 }}>Далі</Text>
+            </TouchableOpacity>
+
+            <StatusBar
+                animated={true}
+                barStyle={'light-content'}
+                backgroundColor="#4BB5F5"
+            />
+        </KeyboardAvoidingView >
+    );
+}
