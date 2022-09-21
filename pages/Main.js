65fa8d4d87f7f8d216d8aa18edfb68a8f1c0b4d7
Ілля <olenchenko.ilya@ukr.net>
2022-09-21 18:56:12 +0300

    main + scroll

--- pages/Main.js
+++ pages/Main.js
@@ -1,20 +1,85 @@
 import { Text, View, StatusBar, Animated, ScrollView, StyleSheet, SafeAreaView } from 'react-native'
 import News from '../components/News'
 
-const App = () => {
-    const dummyDATA = [
-        'Item1', 'Item1', 'Item1', 'Item1', 'Item1', 'Item1', 'Item1', 'Item1', 'Item1', 'Item1',
-        'Item1', 'Item1', 'Item1', 'Item1', 'Item1', 'Item1', 'Item1', 'Item1', 'Item1', 'Item1', 'Item1', 'Item1'
-    ];
-    // const AnimatedHeaderValue = useRef(new Animated.Value(0)).current
+const DATA = [
+    {
+        id: '1',
+        title: 'Нема гарячої води',
+        discription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+        date: '24'
+    },
+    {
+        id: '2',
+        title: 'ВИмкнули світло',
+        discription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+        date: '25'
+    },
+    {
+        id: '3',
+        title: 'Розбито вікна',
+        discription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+        date: '24'
+    },
+    {
+        id: '4',
+        title: 'Торнадо',
+        discription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+        date: '24'
+    },
+    {
+        id: '4',
+        title: 'Війна',
+        discription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+        date: '24'
+    },
+    {
+        id: '4',
+        title: 'Потоп',
+        discription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+        date: '26'
+    },
+    {
+        id: '4',
+        title: 'Авіаудар',
+        discription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+        date: '25'
+    },
+    {
+        id: '4',
+        title: 'Авіаудар',
+        discription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+        date: '25'
+    },
+
+    {
+        id: '4',
+        title: 'Авіаудар',
+        discription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+        date: '25'
+    },
+    {
+        id: '4',
+        title: 'Авіаудар',
+        discription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+        date: '25'
+    },
+    {
+        id: '4',
+        title: 'Авіаудар',
+        discription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+        date: '25'
+    }
+];
+
+export default function Main() {
     let AnimatedHeaderValue = new Animated.Value(0);
 
-    const Header_Max_Height = 250; //Max
+    const Header_Max_Height = 250;
     const Header_Min_Height = 50;
 
     const animateHeaderBackgroundColor = AnimatedHeaderValue.interpolate({
         inputRange: [0, Header_Max_Height - Header_Min_Height],
-        outputRange: ['blue', 'red'],
+        outputRange: ['blue', 'yellow'],
         extrapolate: 'clamp'
     });
 
@@ -26,6 +91,23 @@ const App = () => {
 
     return (
         <SafeAreaView style={styles.container}>
+            <ScrollView
+                style={{ paddingTop: 250, overflow: "hidden", borderRadius: 20 }}
+                scrollEventThrottle={16}
+                onScroll={Animated.event(
+                    [{ nativeEvent: { contentOffset: { y: AnimatedHeaderValue } } }],
+                    { useNativeDriver: false }
+                )}
+            >
+                {
+                    DATA.map((item, index) => (
+                        <Text style={styles.item} key={index}>
+                            {item.title}
+                        </Text>
+                    ))
+                }
+                <View style={{ paddingBottom: 250 }}></View>
+            </ScrollView>
             <Animated.View
                 style={[
                     styles.header,
@@ -37,21 +119,15 @@ const App = () => {
             >
                 <Text style={{ fontSize: 18, textAlign: 'center' }}>Headrt card</Text>
             </Animated.View>
-
-            <ScrollView
-                style={{ paddingTop: 250 }}
-                scrollEventThrottle={16}
-                onScroll={Animated.event(
-                    [{ nativeEvent: { contentOffset: { y: AnimatedHeaderValue } } }],
-                    { useNativeDriver: false }
-                )}
-            >
-                <News />
-            </ScrollView>
+            <StatusBar
+                animated={true}
+                barStyle={'light-content'}
+                backgroundColor="#4BB5F5"
+            />
         </SafeAreaView>
 
     );
-};
+}
 
 const styles = StyleSheet.create({
     container: {
@@ -69,7 +145,13 @@ const styles = StyleSheet.create({
         left: 0,
         right: 0,
         position: 'absolute'
+    },
+    item: {
+        backgroundColor: '#EEEEEE',
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+        borderRadius: 10,
+
     }
 });
-
-export default App;
