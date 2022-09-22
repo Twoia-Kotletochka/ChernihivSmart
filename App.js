4fb3ef050a8af285832527e6bdfbe59f450c5da4
Ілля <olenchenko.ilya@ukr.net>
2022-09-22 19:07:30 +0300

    react-native-gesture-handler +

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Stack from './navigation'
+//import SignUp from './pages/SignUp';
 export default function App() {
   return (
     <Stack />
--- navigation.js
+++ navigation.js
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler'
 import React from 'react'
 import RegistrPassword from './pages/RegistrPassword'
 import LoginPassword from './pages/LoginPassword'
