b9386b5d976ae2e5378179e120479d09a0701585
Ілля <olenchenko.ilya@ukr.net>
2022-09-26 18:53:42 +0300

    app save

--- App.js
+++ App.js
@@ -1,8 +1,30 @@
 import React from 'react'
-import { Image, View, StatusBar, ScrollView, StyleSheet, SafeAreaView } from 'react-native'
+import { Text, Image, View, StatusBar, ScrollView, StyleSheet, SafeAreaView, Animated } from 'react-native'
 import News from './components/News'
 
 export default function Main() {
+  let AnimatedOp = new Animated.Value(0);
+
+  const Max = 100;
+  const Min = 10;
+
+  const animateopacityheader = AnimatedOp.interpolate({
+    inputRange: [0, Max - Min],
+    outputRange: ['gray', 'transparent'],
+    extrapolate: 'clamp'
+  });
+
+  const animateopacityprofil = AnimatedOp.interpolate({
+    inputRange: [0, Max - Min],
+    outputRange: ['red', 'transparent'],
+    extrapolate: 'clamp'
+  });
+
+  const animateopacityweather = AnimatedOp.interpolate({
+    inputRange: [0, Max - Min],
+    outputRange: ['black', 'transparent'],
+    extrapolate: 'clamp'
+  });
 
   return (
     <SafeAreaView style={styles.container}>
@@ -14,13 +36,23 @@ export default function Main() {
 
       <ScrollView
         scrollEventThrottle={16}
-        style={{ paddingTop: 150 }}
+        style={styles.scrollview_vertical}
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { y: AnimatedOp } } }],
+          { useNativeDriver: false }
+        )}
       >
+
         <ScrollView
           horizontal={true}
           style={styles.scrollview_horizontal}
         >
-          <View style={styles.card}></View>
+          <Animated.View style={[styles.card, {
+            backgroundColor: animateopacityheader
+          }]}>
+
+            <Text>sadasd</Text>
+          </Animated.View>
 
           <View style={styles.card}></View>
 
@@ -34,42 +66,53 @@ export default function Main() {
         <View style={styles.view_news}>
           <News />
         </View>
-
       </ScrollView>
+
+      <Animated.View
+        style={[{
+          width: '90%',
+          height: 50,
+          marginTop: 5,
+          position: 'absolute',
+          backgroundColor: 'gray',
+          opacity: 0.8,
+          borderRadius: 50,
+        },
+        { backgroundColor: animateopacityheader }]}>
+
+      </Animated.View>
 
       <View
         style={{
-          width: '100%',
-          height: '6%',
-          marginTop: '2%',
+          width: '90%',
+          marginTop: 5,
           flexDirection: 'row',
           alignContent: 'center',
           justifyContent: 'space-between',
           backgroundColor: 'transparent',
-          position: 'absolute'
+          position: 'absolute',
         }}
       >
-        <View
-          style={{
+        <Animated.View
+          style={[{
             backgroundColor: 'red',
             width: 50,
             height: 50,
-            marginLeft: 20,
-            borderRadius: 100
-          }}
-        ></View>
+            marginLeft: 5,
+            borderRadius: 100,
+          }, { backgroundColor: animateopacityprofil }]}
+        ></Animated.View>
 
-        <View
-          style={{
-            backgroundColor: 'gray',
+        <Animated.View
+          style={[{
+            backgroundColor: 'black',
             width: 50,
             height: 50,
-            marginRight: 20,
-            borderRadius: 100
-          }}
-        ></View>
+            marginRight: 5,
+            borderRadius: 100,
+          }, { backgroundColor: animateopacityweather }]}
+        ></Animated.View>
       </View>
-
 
 
       <StatusBar
@@ -78,22 +121,29 @@ export default function Main() {
         backgroundColor="#4BB5F5"
       />
     </SafeAreaView>
-
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignContent: 'center'
+    alignItems: 'center'
   },
   view_news: {
     borderTopLeftRadius: 20,
     borderTopRightRadius: 20,
     backgroundColor: 'white',
-    paddingBottom: 80
+    paddingBottom: 500
   },
   scrollview_horizontal: {
+    height: '100%',
+    width: '100%',
+    marginTop: 150,
+  },
+  scrollview_vertical: {
+
+    width: '100%',
+    height: '100%',
 
   },
   card: {
