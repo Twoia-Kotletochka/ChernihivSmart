691469f89917f5875a81564d1a35fc786ea53ddf
Ілля <olenchenko.ilya@ukr.net>
2022-09-26 19:44:48 +0300

    card++

--- App.js
+++ App.js
@@ -1,28 +1,48 @@
 import React from 'react'
 import { Text, Image, View, StatusBar, ScrollView, StyleSheet, SafeAreaView, Animated } from 'react-native'
 import News from './components/News'
+import Icon_card from './assets/svg/icon_card.svg'
+import Icon_home from './assets/svg/icon_home.svg'
+import Icon_location from './assets/svg/icon_location.svg'
+import Icon_parking from './assets/svg/icon_parking.svg'
 
 export default function Main() {
   let AnimatedOp = new Animated.Value(0);
 
-  const Max = 100;
-  const Min = 10;
+  const max_opacity = 100;
+  const min_opacity = 10;
+
+  const max_height = 155;
+  const min_height = 0;
 
   const animateopacityheader = AnimatedOp.interpolate({
-    inputRange: [0, Max - Min],
+    inputRange: [0, max_opacity - min_opacity],
     outputRange: ['gray', 'transparent'],
     extrapolate: 'clamp'
   });
 
   const animateopacityprofil = AnimatedOp.interpolate({
-    inputRange: [0, Max - Min],
+    inputRange: [0, max_opacity - min_opacity],
     outputRange: ['red', 'transparent'],
     extrapolate: 'clamp'
   });
 
   const animateopacityweather = AnimatedOp.interpolate({
-    inputRange: [0, Max - Min],
+    inputRange: [0, max_opacity - min_opacity],
     outputRange: ['black', 'transparent'],
+    extrapolate: 'clamp'
+  });
+
+  const animateopacitycard = AnimatedOp.interpolate({
+    inputRange: [0, max_opacity - min_opacity],
+    outputRange: ['white', 'transparent'],
+    extrapolate: 'clamp'
+  });
+
+
+  const animatedCardHeight = AnimatedOp.interpolate({
+    inputRange: [0, max_height - min_height],
+    outputRange: [max_height, min_height],
     extrapolate: 'clamp'
   });
 
@@ -48,19 +68,36 @@ export default function Main() {
           style={styles.scrollview_horizontal}
         >
           <Animated.View style={[styles.card, {
-            backgroundColor: animateopacityheader
+            backgroundColor: animateopacitycard,
+            height: animatedCardHeight,
           }]}>
-
-            <Text>sadasd</Text>
+            <Icon_home style={styles.icon_cards} />
+            <Text style={styles.text_card}>Домівка</Text>
           </Animated.View>
 
-          <View style={styles.card}></View>
+          <Animated.View style={[styles.card, {
+            backgroundColor: animateopacitycard,
+            height: animatedCardHeight,
+          }]}>
+            <Icon_card style={[styles.icon_cards, { transform: [{ scale: 1.3 }], top: 2 }]} />
+            <Text style={styles.text_card}>Транспортна{"\n"}карта</Text>
+          </Animated.View>
 
-          <View style={styles.card}></View>
+          <Animated.View style={[styles.card, {
+            backgroundColor: animateopacitycard,
+            height: animatedCardHeight,
+          }]}>
+            <Icon_location style={styles.icon_cards} />
+            <Text style={styles.text_card}>Мапа{"\n"}транспорту</Text>
+          </Animated.View>
 
-          <View style={styles.card}></View>
-
-          <View style={styles.card}></View>
+          <Animated.View style={[styles.card, {
+            backgroundColor: animateopacitycard,
+            height: animatedCardHeight,
+          }]}>
+            <Icon_parking style={styles.icon_cards} />
+            <Text style={styles.text_card}>Паркування</Text>
+          </Animated.View>
         </ScrollView>
 
         <View style={styles.view_news}>
@@ -141,7 +178,6 @@ const styles = StyleSheet.create({
     marginTop: 150,
   },
   scrollview_vertical: {
-
     width: '100%',
     height: '100%',
 
@@ -154,7 +190,14 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     justifyContent: 'center',
     alignItems: 'center',
+    alignContent: 'center',
     backgroundColor: 'white',
     borderRadius: 17,
+  },
+  text_card: {
+    textAlign: 'center'
+  },
+  icon_cards: {
+    width: '40%', height: '40%'
   }
 })
