9db760a8cc4fd1afaeac150dabeddb83eaac6aa8
Ілля <olenchenko.ilya@ukr.net>
2022-09-27 19:42:23 +0300

    arrow и будет финал

--- components/News.js
+++ components/News.js
@@ -1,92 +1,155 @@
 import React from 'react';
-import { StyleSheet, Text } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import Icon_water from '../assets/icon_news_svg/water.svg'
+import Icon_arrow from '../assets/icon_news_svg/arrow.svg'
 
 const DATA = [
     {
         id: '1',
-        title: 'Нема гарячої води',
+        icon: 'light',
+        title1: 'Нема гарячої води',
+        title2: 'Не панікуйте',
+        title3: 'Дякуємо за очікування',
         discription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-        date: '24'
+        date: 'годину тому'
     },
     {
         id: '2',
-        title: 'ВИмкнули світло',
+        icon: 'light',
+        title1: 'Нема гарячої води',
+        title2: 'Не панікуйте',
+        title3: 'Дякуємо за очікування',
         discription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-        date: '25'
+        date: 'годину тому'
     },
     {
         id: '3',
-        title: 'Розбито вікна',
+        icon: 'light',
+        title1: 'Нема гарячої води',
+        title2: 'Не панікуйте',
+        title3: 'Дякуємо за очікування',
         discription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-        date: '24'
+        date: 'годину тому'
     },
     {
         id: '4',
-        title: 'Торнадо',
+        icon: 'light',
+        title1: 'Нема гарячої води',
+        title2: 'Не панікуйте',
+        title3: 'Дякуємо за очікування',
         discription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-        date: '24'
+        date: 'годину тому'
     },
     {
-        id: '4',
-        title: 'Війна',
+        id: '5',
+        icon: 'light',
+        title1: 'Нема гарячої води',
+        title2: 'Не панікуйте',
+        title3: 'Дякуємо за очікування',
         discription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-        date: '24'
+        date: 'годину тому'
     },
     {
-        id: '4',
-        title: 'Потоп',
+        id: '6',
+        icon: 'light',
+        title1: 'Нема гарячої води',
+        title2: 'Не панікуйте',
+        title3: 'Дякуємо за очікування',
         discription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-        date: '26'
+        date: 'годину тому'
     },
     {
-        id: '4',
-        title: 'Авіаудар',
+        id: '7',
+        icon: 'light',
+        title1: 'Нема гарячої води',
+        title2: 'Не панікуйте',
+        title3: 'Дякуємо за очікування',
         discription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-        date: '25'
+        date: 'годину тому'
     },
     {
-        id: '4',
-        title: 'Авіаудар',
+        id: '8',
+        icon: 'light',
+        title1: 'Нема гарячої води',
+        title2: 'Не панікуйте',
+        title3: 'Дякуємо за очікування',
         discription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-        date: '25'
-    },
-
-    {
-        id: '4',
-        title: 'Авіаудар',
-        discription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-        date: '25'
+        date: 'годину тому'
     },
     {
-        id: '4',
-        title: 'Авіаудар',
+        id: '9',
+        icon: 'light',
+        title1: 'Нема гарячої води',
+        title2: 'Не панікуйте',
+        title3: 'Дякуємо за очікування',
         discription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-        date: '25'
+        date: 'годину тому'
     },
     {
-        id: '4',
-        title: 'Авіаудар',
+        id: '10',
+        icon: 'light',
+        title1: 'Нема світла',
+        title2: 'Панікуйте',
+        title3: 'Дякуємо за очікування',
         discription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-        date: '25'
-    }
+        date: 'годину тому'
+    },
 ];
 
 const News = () => {
-    return (
-        DATA.map((item, index) => (
-            <Text style={styles.item} key={index}>
-                {item.title}
-            </Text>
-        ))
+    return (DATA.map((item, index) => (
+        <View style={styles.container} key={index}>
+            <View style={{ marginLeft: 5, paddingTop: 15 }}>
+                <View style={styles.icon_board}>
+                    <Icon_water style={{ width: '80%', height: '80%' }} />
+                </View>
+
+                <Text style={{ fontSize: 10, paddingTop: 15, paddingBottom: 2 }}>
+                    {item.date}
+                </Text>
+            </View>
+            <View style={{ flexDirection: 'column', paddingTop: 5 }}>
+                <Text style={styles.title1}>
+                    {item.title1}
+                </Text>
+                <Text style={styles.title2}>
+                    {item.title2}
+                </Text>
+                <Text style={styles.title3}>
+                    {item.title3}
+                </Text>
+            </View>
+
+        </View>
+    ))
     );
 }
 export const styles = StyleSheet.create({
-    item: {
+    container: {
         backgroundColor: '#EEEEEE',
-        padding: 20,
         marginVertical: 8,
         marginHorizontal: 16,
-        borderRadius: 10
+        borderRadius: 10,
+        flexDirection: 'row',
+    },
+    title1: {
+        fontSize: 20,
+    },
+    title2: {
+        fontSize: 15,
+    },
+    title3: {
+        fontSize: 10,
+    },
+    icon_board: {
+        width: 50,
+        height: 50,
+        backgroundColor: 'white',
+        borderRadius: 100,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginRight: 20,
+
     }
 })
 
