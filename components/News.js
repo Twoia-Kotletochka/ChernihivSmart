e44b5213bc7142d3f225315d50b706ee24d8e205
Ilya-Olenchenko <olenchenko.ilya@ukr.net>
2023-03-03 18:52:28 +0200

    1

--- components/News.js
+++ components/News.js
@@ -1,11 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native';
 import Icon_water from '../assets/icon_news_svg/water.svg'
 import Icon_arrow from '../assets/icon_news_svg/arrow.svg'
 
 import { firebase } from '../config'
-
-
 import { Component } from 'react/cjs/react.development';
 
 const DATA = [
@@ -101,69 +99,60 @@ const DATA = [
     },
 ];
 
+const database = firebase.database();
 
-class News extends Component {
+const News = () => {
+    const [data, setData] = useState(null);
 
-    constructor(props) {
-        super(props);
-        this.state = {
-          name:[]
-        }
-      }
-      
-    componentDidMount() {
-        // const reference = firebase.database().ref('/User')
-        // reference.on('value', snapshot =>{
-        //     console.log('User data: ', snapshot.val());
-        //     this.setState({
-        //         name:snapshot.val(),
-        //     });
-        // });
+    useEffect(() => {
+        database.ref('news/').once('value').then((snapshot) => {
+            setData(snapshot.val());
+        });
+    }, []);
 
-        // firebase.database()
-        // .ref('/')
-        // .on('value', snapshot =>{
-        //     this.setState({
-        //         name:snapshot.val()
-        //     })
-        // });
-
-        
+    if (!data) {
+        return <View><Text>Loading...</Text></View>;
     }
 
-    render() {
-        // return (Object.keys(items).map((key) => (
-        //     <View style={styles.container} key={key}>
-        //         <View style={{ marginLeft: 5, paddingTop: 15 }}>
-        //             <View style={styles.icon_board}>
-        //                 <Icon_water style={{ width: '80%', height: '80%' }} />
-        //             </View>
-
-        //             <Text style={{ fontSize: 10, paddingTop: 15, paddingBottom: 2 }}>
-        //                 {items[key].name}
-        //             </Text>
-        //         </View>
-        //         {/* <View style={{ flexDirection: 'column', paddingTop: 5 }}>
-        //             <Text style={styles.title1}>
-        //                 {item.title1}
-        //             </Text>
-        //             <Text style={styles.title2}>
-        //                 {item.title2}
-        //             </Text>
-        //             <Text style={styles.title3}>
-        //                 {item.title3}
-        //             </Text>
-        //         </View> */}
-
-        //     </View>
-        // )));
-        return(
-            <View>
-                <Text >{this.state.name}</Text>
-            </View>
+    const databaseContent = [];
+    Object.keys(data).forEach((key) => {
+        const value = data[key];
+        databaseContent.push(
+            <Text key={key}>
+                id: {value.id}, name: {value.name}
+            </Text>
         );
-    }
+    });
+
+    return (
+        <View>{databaseContent}</View>
+    );
 }
+
+// return (Object.keys(items).map((key) => (
+//     <View style={styles.container} key={key}>
+//         <View style={{ marginLeft: 5, paddingTop: 15 }}>
+//             <View style={styles.icon_board}>
+//                 <Icon_water style={{ width: '80%', height: '80%' }} />
+//             </View>
+//             <Text style={{ fontSize: 10, paddingTop: 15, paddingBottom: 2 }}>
+//                 {items[key].name}
+//             </Text>
+//         </View>
+//         {/* <View style={{ flexDirection: 'column', paddingTop: 5 }}>
+//             <Text style={styles.title1}>
+//                 {item.title1}
+//             </Text>
+//             <Text style={styles.title2}>
+//                 {item.title2}
+//             </Text>
+//             <Text style={styles.title3}>
+//                 {item.title3}
+//             </Text>
+//         </View> */}
+//     </View>
+// )));
+
 export const styles = StyleSheet.create({
     container: {
         backgroundColor: '#EEEEEE',
